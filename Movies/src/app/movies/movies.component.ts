959219385f7aa77864ce81cbe77872554b14808e
Crackinator <87207476+Crackinator@users.noreply.github.com>
2023-10-15 17:37:11 +0200

    4th commit

--- Movies/src/app/movies/movies.component.ts
+++ Movies/src/app/movies/movies.component.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { WatchlistService } from '../watchlist/watchlist.service';
 import { Movie } from '../movie';
 
-
 @Component({
   selector: 'mov-movies',
   templateUrl: './movies.component.html',
@@ -19,17 +18,15 @@ export class MoviesComponent implements OnInit {
   constructor(private watchlistService: WatchlistService) {}
 
   ngOnInit(): void {
-    // You can initialize your movies array here if needed
   }
 
-  // Update the searchMovies method to fetch data and assign it to the movies array
+  // fetching data and put it into the movies Array
   async searchMovies(event: Event) {
     event.preventDefault();
   
     if (!this.searchTerm) {
-      // Handle the case where the search term is empty
-      console.log('Please enter a valid movie name.');
-      return;
+      // error if the movie Data could not be fetched 
+      console.log('An error occurred while fetching data.');
     }
   
     const apiKey = `http://www.omdbapi.com/?apikey=288a03da&t=${encodeURIComponent(this.searchTerm)}`;
@@ -39,10 +36,12 @@ export class MoviesComponent implements OnInit {
       if (response.data.Response === 'True') {
         this.movies = [response.data];
         this.showPlot = false;
+        this.showError = false;
       } else {
         this.showError = true
-        this.errorMessage = 'An error occurred while fetching data.';
-        console.log('Movie not found.');
+        this.errorMessage = 'No Movie called "'+ this.searchTerm + '"';
+        alert(this.errorMessage);
+        return console.log('Movie not found.'); 
       }
     } catch (error) {
       console.log('Fehler beim Abrufen der Filmdaten:', error);
@@ -51,12 +50,12 @@ export class MoviesComponent implements OnInit {
   }
   
 
-  // Add a movie to the watchlist
+  // add a movie to the watchlist
   addToWatchlist(movie: Movie): void {
     this.watchlistService.addToWatchlist(movie);
   }
 
-  // Toggle the plot display
+  // toggle the plot display
   togglePlot(): void {
     this.showPlot = !this.showPlot;
   }
